c8f812543a0f83d65ccb3fcc7fc0a5bf7a0e08f7
anjum18 <anjumandleeb@gmail.com>
2018-10-31 13:44:35 +0100

    ok

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <title>
+    <?php echo $pagetitle; ?>
+</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="templette.css">
+
+    <title>table de multiplication</title>
+  </head>
+  <body>
+       
+   
+    <nav  id="menu"class="navbar navbar-expand-md navbar-dark fixed-top">
+            <a class="navbar-brand" href="#">PHP Table</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+      
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">exercise no 1 <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">exercise no 2</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">exercise no 3</a>
+                </li>
+                
+              </ul>
+              
+            </div>
+          </nav>
+      
+          <main >
+      
+              <div >
+                    <h1 class="main title">Table de multiplication</h1>
+                    <img id="cover"src="cover.jpg" alt="image de multiplication">
+                  
+                </div>
+        
+           
+            
+      
+      
+          </main>
+      
+          <footer >
+            <p>&copy; Company 2017-2018</p>
+          </footer>
+          
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
